cf1063d54409d61d163ec879c4bad1a0064da9cb
Christian Anderson <ca96187@gmail.com>
2019-09-09 15:16:23 -0600

    refactored code to be more readable

--- space-age.js
+++ space-age.js
@@ -1,16 +1,17 @@
-const orbitalPeriods = function () {
-  let earth = 31557600
+const earthYearInSeconds = 31557600
+
+const orbitalPeriodComparedToEarth = () => {
   return {
-    earth,
-    mercury: 0.2408467 * earth,
-    venus: 0.61519726 * earth,
-    mars: 1.8808158 * earth,
-    jupiter: 11.862615 * earth,
-    saturn: 29.447498 * earth,
-    uranus: 84.016846 * earth,
-    neptune: 164.79132 * earth
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   }
 }
 
-export const age = (planet, ageInSeconds) => +((ageInSeconds / orbitalPeriods()[planet]).toFixed(2))
+export const age = (planet, ageInSeconds) => Number((ageInSeconds / (orbitalPeriodComparedToEarth()[planet] * earthYearInSeconds)).toFixed(2))
 
