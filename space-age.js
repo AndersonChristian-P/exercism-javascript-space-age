3842306d051607bff186e83be41f2c1cbe473989
Christian Anderson <ca96187@gmail.com>
2019-09-11 11:59:31 -0600

    changed orbitalPeriodComparedToEarth from a function to a constant

--- space-age.js
+++ space-age.js
@@ -1,17 +1,15 @@
 const earthYearInSeconds = 31557600
 
-const orbitalPeriodComparedToEarth = () => {
-  return {
-    earth: 1,
-    mercury: 0.2408467,
-    venus: 0.61519726,
-    mars: 1.8808158,
-    jupiter: 11.862615,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132
-  }
+const orbitalPeriodComparedToEarth = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
 }
 
-export const age = (planet, ageInSeconds) => Number((ageInSeconds / (orbitalPeriodComparedToEarth()[planet] * earthYearInSeconds)).toFixed(2))
+export const age = (planet, ageInSeconds) => Number((ageInSeconds / (orbitalPeriodComparedToEarth[planet] * earthYearInSeconds)).toFixed(2))
 
